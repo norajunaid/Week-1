e9dec36c6a3d30d89d9ae50ae744a87d16e8a6ff
Nora Junaid <njunaid@umass.edu>
2021-09-09 13:24:54 -0400

    Initial Commit

---
+++ Week 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week 1
+//
+//  Created by Nora Junaid on 9/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Week 1Tests/Week_1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Week_1Tests.swift
+//  Week 1Tests
+//
+//  Created by Nora Junaid on 9/9/21.
+//
+
+import XCTest
+@testable import Week_1
+
+class Week_1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
